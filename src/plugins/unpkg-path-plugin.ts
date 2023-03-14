6df6e18288852f2bfb30a865fdd8da1fcc574667
hellojoelhuber <hello@joelhuber.com>
2023-03-14 12:02:12 -0500

    fixing pulling from unpkg and starting to add caching

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,12 +1,32 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+    name: 'filecache'
+})
 
 export const unpkgPathPlugin = () => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
             build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log('onResole', args);
-                return { path: args.path, namespace: 'a' };
+                console.log('onResolve', args);
+                if (args.path === 'index.js') {
+                    return { path: args.path, namespace: 'a' };
+                } 
+
+                if (args.path.includes('./') || args.path.includes('../')) {
+                    return {
+                        namespace: 'a',
+                        path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
+                    }
+                }
+
+                return {
+                    namespace: 'a',
+                    path: `https://unpkg.com/${args.path}`
+                }
             });
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,11 +36,33 @@ export const unpkgPathPlugin = () => {
                     return {
                     loader: 'jsx',
                     contents: `
-                        import message from 'tiny-test-pkg';
+                        const message = require('nested-test-pkg');
                         console.log(message);
                     `,
                     };
                 } 
+
+                // Check if file already fetched & in cache
+                const cachedResult = await fileCache.getItem(args.path)
+
+                // if so, return immediately
+                if (cachedResult) {
+                    return cachedResult
+                }
+
+                // else request file
+                const { data, request } = await axios.get(args.path)
+                
+                const result = {
+                    loader: 'jsx',
+                    contents: data,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                }
+
+                // and store response in cache
+                await fileCache.setItem(args.path, result)
+
+                return result
             });
         },
     };
